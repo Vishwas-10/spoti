584f2de506baa35539bebf6a0209deba476974fd
Vishwas Saini <79042063+Vishwas-10@users.noreply.github.com>
2023-09-19 21:20:47 +0530

    Update oauth.py

--- oauth.py
+++ oauth.py
@@ -6,7 +6,7 @@ class SpotifyOauthClient():
 
         self.CLIENT_ID = "593afdf556c84f6eb44315ba64305fd2"
         self.CLIENT_SECRET = "c2741ecbab2042ed8bfd021ec0f60c81"
-        self.REDIRECT_URI = "http://127.0.0.1:5000/redirect/"
+        self.REDIRECT_URI = "https://spoti-enhancer.onrender.com/redirect/"
         self.SCOPE = "user-top-read user-read-private user-follow-read playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private"
         self.OAUTH_AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
         self.OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
